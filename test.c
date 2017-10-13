d16e625eab4a094907df0e6cf4c4e5e12c55e781
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-10-12 22:35:59 -0300

    particle moving

--- test.c
+++ test.c
@@ -3,34 +3,62 @@
 #include <stdio.h>
 #include <unistd.h>
 
-double PARTICLE_SIZE = 0.9;
+#define DEBUG 0
+
+FILE *fp;
+
+struct particle {
+    double x;
+    double y;
+    double radius;
+};
+
+typedef struct particle Particle ;
+
+Particle currentParticle;
+
+void drawParticle(Particle particle)
+{
+    glBegin(GL_POLYGON);
+        for(double i = 0; i < 2 * 3.1415; i += 3.1415 / 18) {
+            if (DEBUG) {
+                fprintf(fp, "i=%f\n", i);
+            }
+            glVertex3f(particle.x +  (cos(i) * particle.radius), particle.y + (sin(i) * particle.radius), 0.0);
+        }
+        if (DEBUG) {
+            fprintf(fp, "END===\n");
+        }
+}
 
 void renderScene(void) {
-    FILE *fp;
-    fp = fopen("/tmp/foo", "a");
+    if (DEBUG)  {
+        fp = fopen("/tmp/debug", "a");
+    }
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBegin(GL_POLYGON);
-        for(double i = 0; i < 2 * 3.1415; i += 3.1415 / 4) {
-            fprintf(fp, "i=%f\n", i);
-            glVertex3f(cos(i) * PARTICLE_SIZE, sin(i) * PARTICLE_SIZE, 0.0);
-        }
-        fprintf(fp, "END===\n");
+    drawParticle(currentParticle);
     glEnd();
 
     glutSwapBuffers();
-    fclose(fp);
+    if (DEBUG ) {
+        fclose(fp);
+    }
 }
 
 void update(void) {
-    /* PARTICLE_SIZE-= PARTICLE_SIZE*0.01; */
-    /* glutPostRedisplay (); */
+    /* currentParticle.radius+= currentParticle.radius*0.1; */
+    currentParticle.x+=0.01;
+    glutPostRedisplay();
 }
 
 int
 main(int argc, char **argv)
 {
+  currentParticle.x = 0.7;
+  currentParticle.y = 0.2;
+  currentParticle.radius = 0.01;
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(800,600);
