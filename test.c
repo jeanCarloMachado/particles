56218ba0abaed213d42334788f21bcf06b2de5d6
Jean Carlo Machado <contato@jeancarlomachado.com.br>
2017-10-12 22:04:43 -0300

    drawing circle

--- test.c
+++ test.c
@@ -1,17 +1,31 @@
 #include <GL/glut.h>
 #include <math.h>
+#include <stdio.h>
+#include <unistd.h>
+
+double PARTICLE_SIZE = 0.9;
 
 void renderScene(void) {
+    FILE *fp;
+    fp = fopen("/tmp/foo", "a");
 
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     glBegin(GL_POLYGON);
-		glVertex3f(-0.9,-0.9,0.0);
-		glVertex3f(0.9,0.0,0.0);
-		glVertex3f(0.0,0.9,0.0);
-		glVertex3f(-0.9,-0.9,0.0);
-	glEnd();
-	glutSwapBuffers();
+        for(double i = 0; i < 2 * 3.1415; i += 3.1415 / 4) {
+            fprintf(fp, "i=%f\n", i);
+            glVertex3f(cos(i) * PARTICLE_SIZE, sin(i) * PARTICLE_SIZE, 0.0);
+        }
+        fprintf(fp, "END===\n");
+    glEnd();
+
+    glutSwapBuffers();
+    fclose(fp);
+}
+
+void update(void) {
+    /* PARTICLE_SIZE-= PARTICLE_SIZE*0.01; */
+    /* glutPostRedisplay (); */
 }
 
 int
@@ -20,8 +34,9 @@ main(int argc, char **argv)
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
   glutInitWindowSize(800,600);
-  glutCreateWindow("teste");
+  glutCreateWindow("Particles");
   glutDisplayFunc(renderScene);
+  glutIdleFunc(update);
   glutMainLoop();
   return 0;             /* ANSI C requires main to return int. */
 }
